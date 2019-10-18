72930980f15d991044586afde8166eff48e3ebbb
Your Name <you@example.com>
2019-10-18 13:20:18 +0700

    add symbol file

--- array.rb
+++ array.rb
@@ -4,10 +4,13 @@ require "awesome_print"
 # ARRAY
 animal = ['cat', 'dog', 'rat', 'fish', 'monkey']
 
+# ap animal.count
+# ap animal.length
 # ap animal[-1] #monkey
 # ap animal[0, 0] # print nothing
 # ap animal[0, 2] # cat dog
 # ap animal.slice(0, 2) # cat dog
+# ap animal.take(2) # cat dog
 # ap animal[0..2] # cat dog rat
 # ap animal.[] 0, 2 # cat dog
 # ap animal.[]0,2.to_s # >>>>> ERROR!
@@ -17,43 +20,11 @@ animal = ['cat', 'dog', 'rat', 'fish', 'monkey']
 # ap animal.reverse
 # ap animal.push('keong')
 # ap animal << 'keong'
+# ap animal.unshift('monkey')
+# ap animal.insert(1, 'penyelinap')
+# animal.pop
+# animal.shift
 # ap animal.include?('fish')
 # ap animal - ['rat', 'fish', 'monkey'] # cat dog
 # ap [[1,2,3], [4,5,6], 7, [[8,9], 10]].flatten, :multiline => false
 # ap [ 10, 2, 2, 6, 5, 6, 2, 3, 9, 10 ].uniq, :multiline => false
-
-# Symbol
-# :data1
-{data1: "oke"}
-# ap Symbol.all_symbols.grep(/data/), multiline: false
-# ap animal.grep(/a/), multiline: false
-# ap animal % ['cat', 'dog']
-
-# HASH
-# gem install awesome_print
-
-people = {
-  name: 'John',
-  :age => 25,
-  :address => {
-    :city => 'Jakarta',
-    :country => 'Indonesia'
-  }
-}
-
-data = {
-  "id": 1,
-  "name": "John Doe",
-  "age": 23,
-  "address": {
-    "country": "Jakarta",
-    "city": "Indonesia"
-  }
-}
-
-# data JSON with puts JSON.pretty_generate(data)
-# puts people, :multiline => true
-
-# ap people[:name]
-# ap people
-# ap ''.methods - Object.methods
---
+++ symbol.rb
@@ -0,0 +1,36 @@
+# Symbol
+
+# :data1
+{data1: "oke"}
+# ap Symbol.all_symbols.grep(/data/), multiline: false
+# ap animal.grep(/a/), multiline: false
+# ap animal % ['cat', 'dog']
+
+# HASH
+# gem install awesome_print
+
+people = {
+  name: 'John',
+  :age => 25,
+  :address => {
+    :city => 'Jakarta',
+    :country => 'Indonesia'
+  }
+}
+
+data = {
+  "id": 1,
+  "name": "John Doe",
+  "age": 23,
+  "address": {
+    "country": "Jakarta",
+    "city": "Indonesia"
+  }
+}
+
+# data JSON with puts JSON.pretty_generate(data)
+# puts people, :multiline => true
+
+# ap people[:name]
+# ap people
+# ap ''.methods - Object.methods
